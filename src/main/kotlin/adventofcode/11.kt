f1cf63fee72dd6f07e1ffa93a5f4ad4f6dc56d82
Magnus Andersson <mandersson@invidi.se>
2018-12-11 18:11:30 +0100

    Day 11, Viola Jones algorithm

--- src/main/kotlin/adventofcode/11.kt
+++ src/main/kotlin/adventofcode/11.kt
@@ -16,21 +16,55 @@ fun main(args: Array<String>) {
         fuelCells.add(row)
     }
 
-    val mostTotalPowerFuelCellPerGrid = mutableListOf<Pair<FuelCell,Int>>()
-    for (squareSize in 1..12){
+
+    // Viola Jones: https://www.youtube.com/watch?v=uEJ71VlUmMQ
+    val preCalculatedBoxes: HashMap<Pair<Int, Int>, Int> = hashMapOf()
+    for (y in 1..gridSize) {
+        for (x in 1..gridSize) {
+            preCalculatedBoxes.put(
+                Pair(x, y),
+                preCalculatedBoxes.getOrDefault(Pair(x - 1, y), 0) +
+                        preCalculatedBoxes.getOrDefault(Pair(x, y - 1), 0) -
+                        preCalculatedBoxes.getOrDefault(Pair(x - 1, y - 1), 0) +
+                        fuelCells[y - 1][x - 1].powerLevel
+            )
+        }
+    }
+
+    val mostTotalPowerFuelCellPerGrid = mutableListOf<Pair<FuelCell, Int>>()
+    for (squareSize in 1..gridSize) {
         for (y in 1..gridSize - (squareSize - 1)) {
             for (x in 1..gridSize - (squareSize - 1)) {
-                calculateTotalPower(x - 1, y - 1, fuelCells, squareSize)
+                //calculateTotalPower(x - 1, y - 1, fuelCells, squareSize)
+                violaJones(fuelCells, y, x, preCalculatedBoxes, squareSize)
             }
         }
 
         val mostTotalPowerFuelCell = fuelCells.flatten().sortedByDescending { it.totalPower }[0]
         mostTotalPowerFuelCellPerGrid.add(Pair(mostTotalPowerFuelCell.copy(), squareSize))
+
+        //println("FuelCell: $mostTotalPowerFuelCell $squareSize")
     }
 
     mostTotalPowerFuelCellPerGrid.sortByDescending { it.first.totalPower }
 
     println("FuelCell: " + mostTotalPowerFuelCellPerGrid[0])
+}
+
+private fun violaJones(
+    fuelCells: MutableList<MutableList<FuelCell>>,
+    y: Int,
+    x: Int,
+    preCalculatedBoxes: HashMap<Pair<Int, Int>, Int>,
+    squareSize: Int
+) {
+   val sum =
+            preCalculatedBoxes.getOrDefault(Pair(x + squareSize - 1, y + squareSize - 1), 0) -
+            preCalculatedBoxes.getOrDefault(Pair(x - 1, y + squareSize - 1), 0) -
+            preCalculatedBoxes.getOrDefault(Pair(x + squareSize - 1, y - 1), 0) +
+            preCalculatedBoxes.getOrDefault(Pair(x - 1, y - 1), 0)
+
+    fuelCells[y - 1][x - 1].totalPower = sum
 }
 
 data class FuelCell(val x: Int, val y: Int, val powerLevel: Int, var totalPower: Int = 0)
