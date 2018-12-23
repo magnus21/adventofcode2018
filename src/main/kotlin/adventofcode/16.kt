ec5dd939a5b44fdd453b54bbc9fdf1ea55c4a7bf
Magnus Andersson <mandersson@invidi.se>
2018-12-23 18:09:24 +0100

    Day 19.

--- src/main/kotlin/adventofcode/16.kt
+++ src/main/kotlin/adventofcode/16.kt
@@ -4,139 +4,152 @@ import java.io.File
 
 fun main(args: Array<String>) {
 
-    val rawInput = File("src/main/resources/16.txt").readLines()
-    val (operationResults, instructions) = parseInput(rawInput)
+    val operationCodes = OperationCodes()
+
+    val (operationResults, instructions) = operationCodes.parseInput()
 
     //println(operationResults)
     //println(registries)
 
-    val operations = getOperations()
+    val operations = operationCodes.getOperations()
 
-    val partOneAnswer = operationResults.map {  operationResult ->
+    val partOneAnswer = operationResults.map { operationResult ->
         val ins = operationResult.instruction
         operations.filter { operation ->
             val registry = operationResult.beforeState.toMutableList()
             operation.value(ins.a, ins.b, ins.c, registry)
             registry == operationResult.afterState
-            }.size
+        }.size
     }.filter { it >= 3 }.size
 
     println(partOneAnswer)
 
-    val opCodes = operationResults.map { it.instruction.opCode }.distinct().sorted()
-
-    var operationsMatched = opCodes.map { opCode ->
-        val matchingOperationsResults = operationResults
-            .filter { it.instruction.opCode == opCode }
-
-        val matchingOperations = operations.filter { operation ->
-            matchingOperationsResults.all {operationResult ->
-                val ins = operationResult.instruction
-                val registry = operationResult.beforeState.toMutableList()
-                operation.value(ins.a, ins.b, ins.c, registry)
-                registry == operationResult.afterState
-            }
-        }
-
-        Pair(opCode, matchingOperations.map { it.key })
-    }
-
-    // Reduce
-    while(operationsMatched.any{ it.second.size > 1 }){
-        val oneMatches = operationsMatched
-            .filter { it.second.size == 1 }.toMutableList()
-
-        val oneOps = operationsMatched
-            .filter { it.second.size == 1 }
-            .flatMap { it.second }
-
-        val reduced =operationsMatched
-            .filter { it.second.size > 1 }
-            .map { Pair(it.first,it.second.filter { op -> !oneOps.contains(op) })}
-
-        oneMatches.addAll(reduced)
-        operationsMatched = oneMatches
-    }
-
-    val opCodeMap = operationsMatched.map { Pair(it.first,it.second[0]) }
+    val opCodeMap = operationCodes.getOpCodes(operationResults, operations)
 
     println(opCodeMap)
 
-    val registry = mutableListOf(0,0,0,0)
-    for(ins in instructions) {
-        val operation = operations[opCodeMap.filter { it.first == ins.opCode}.map { it.second }.first()]
+    val registry = mutableListOf(0L, 0L, 0L, 0L)
+    for (ins in instructions) {
+        val operation = operations[opCodeMap.filter { it.first == ins.opCode }.map { it.second }.first()]
         operation?.invoke(ins.a, ins.b, ins.c, registry)
     }
 
     println(registry)
 }
 
-data class OperationResult(var beforeState: List<Int>, var afterState: List<Int>, var instruction: Instruction)
-data class Instruction(val opCode: Int, val a: Int, val b: Int, val c: Int)
+data class OperationResult(var beforeState: List<Long>, var afterState: List<Long>, var instruction: Instruction)
+data class Instruction(val opCode: Long, val a: Long, val b: Long, val c: Long)
 
+class OperationCodes {
 
-fun parseInput(rawInput: List<String>): Pair<List<OperationResult>, List<Instruction>> {
-    val operations = mutableListOf<OperationResult>()
-    val instructions = mutableListOf<Instruction>()
+    fun parseInput(): Pair<List<OperationResult>, List<Instruction>> {
 
-    var count = 0
-    while (rawInput[count].isNotEmpty()) {
+        val rawInput = File("src/main/resources/16.txt").readLines()
 
-        val before = parseRegistry(rawInput[count])
-        val opcode = parseInstruction(rawInput[count + 1])
-        val after = parseRegistry(rawInput[count + 2])
+        val operations = mutableListOf<OperationResult>()
+        val instructions = mutableListOf<Instruction>()
 
-        operations.add(OperationResult(before, after, opcode))
+        var count = 0
+        while (rawInput[count].isNotEmpty()) {
 
-        count += 4
-    }
+            val before = parseRegistry(rawInput[count])
+            val opcode = parseInstruction(rawInput[count + 1])
+            val after = parseRegistry(rawInput[count + 2])
 
-    while (count++ < rawInput.size - 1) {
-        if (rawInput[count].isBlank()) {
-            continue
+            operations.add(OperationResult(before, after, opcode))
+
+            count += 4
         }
 
-        instructions.add(parseInstruction(rawInput[count]))
+        while (count++ < rawInput.size - 1) {
+            if (rawInput[count].isBlank()) {
+                continue
+            }
+
+            instructions.add(parseInstruction(rawInput[count]))
+        }
+
+        return Pair(operations, instructions)
     }
 
-    return Pair(operations, instructions)
-}
+    fun parseInstruction(input: String): Instruction {
+        val list = input.split(" ").map { Integer.valueOf(it).toLong() }.toList()
+        return Instruction(list[0], list[1], list[2], list[3])
+    }
 
-fun parseInstruction(input: String): Instruction {
-    val list = input.split(" ").map { Integer.valueOf(it) }.toList()
-    return Instruction(list[0], list[1], list[2], list[3])
-}
+    fun parseRegistry(input: String): List<Long> {
+        return input.substring(9, input.length - 1).split(", ").map { Integer.valueOf(it).toLong() }.toList()
+    }
 
-fun parseRegistry(input: String): List<Int> {
-    return input.substring(9, input.length - 1).split(", ").map { Integer.valueOf(it) }.toList()
-}
+    fun getOpCodes(
+        operationResults: List<OperationResult>,
+        operations: MutableMap<String, (Long, Long, Long, MutableList<Long>) -> Unit>
+    ): List<Pair<Long, String>> {
+        val opCodes = operationResults.map { it.instruction.opCode }.distinct().sorted()
 
-fun getOperations(): MutableMap<String, (Int, Int, Int, MutableList<Int>) -> Unit> {
-    val operations = mutableMapOf<String, (Int, Int, Int, MutableList<Int>) -> Unit>()
+        var operationsMatched = opCodes.map { opCode ->
+            val matchingOperationsResults = operationResults
+                .filter { it.instruction.opCode == opCode }
 
-    operations["addr"] = { a, b, c, registry -> registry[c] = registry[a] + registry[b] }
-    operations["addi"] = { a, b, c, registry -> registry[c] = registry[a] + b }
+            val matchingOperations = operations.filter { operation ->
+                matchingOperationsResults.all { operationResult ->
+                    val ins = operationResult.instruction
+                    val registry = operationResult.beforeState.toMutableList()
+                    operation.value(ins.a, ins.b, ins.c, registry)
+                    registry == operationResult.afterState
+                }
+            }
 
-    operations["mulr"] = { a, b, c, registry -> registry[c] = registry[a] * registry[b] }
-    operations["muli"] = { a, b, c, registry -> registry[c] = registry[a] * b }
+            Pair(opCode, matchingOperations.map { it.key })
+        }
 
-    operations["banr"] = { a, b, c, registry -> registry[c] = registry[a].and(registry[b]) }
-    operations["bani"] = { a, b, c, registry -> registry[c] = registry[a].and(b) }
+        // Reduce
+        while (operationsMatched.any { it.second.size > 1 }) {
+            val oneMatches = operationsMatched
+                .filter { it.second.size == 1 }.toMutableList()
 
-    operations["borr"] = { a, b, c, registry -> registry[c] = registry[a].or(registry[b]) }
-    operations["bori"] = { a, b, c, registry -> registry[c] = registry[a].or(b) }
+            val oneOps = operationsMatched
+                .filter { it.second.size == 1 }
+                .flatMap { it.second }
 
-    operations["setr"] = { a, b, c, registry -> registry[c] = registry[a] }
-    operations["seti"] = { a, b, c, registry -> registry[c] = a }
+            val reduced = operationsMatched
+                .filter { it.second.size > 1 }
+                .map { Pair(it.first, it.second.filter { op -> !oneOps.contains(op) }) }
 
-    operations["gtir"] = { a, b, c, registry -> registry[c] = if (a > registry[b]) 1 else 0 }
-    operations["gtri"] = { a, b, c, registry -> registry[c] = if (registry[a] > b) 1 else 0 }
-    operations["gtrr"] = { a, b, c, registry -> registry[c] = if (registry[a] > registry[b]) 1 else 0 }
+            oneMatches.addAll(reduced)
+            operationsMatched = oneMatches
+        }
 
-    operations["eqir"] = { a, b, c, registry -> registry[c] = if (a == registry[b]) 1 else 0 }
-    operations["eqri"] = { a, b, c, registry -> registry[c] = if (registry[a] == b) 1 else 0 }
-    operations["eqrr"] = { a, b, c, registry -> registry[c] = if (registry[a] == registry[b]) 1 else 0 }
+        return operationsMatched.map { Pair(it.first, it.second[0]) }
+    }
+
+    fun getOperations(): MutableMap<String, (Long, Long, Long, MutableList<Long>) -> Unit> {
+        val operations = mutableMapOf<String, (Long, Long, Long, MutableList<Long>) -> Unit>()
+
+        operations["addr"] = { a, b, c, registry -> registry[c.toInt()] = registry[a.toInt()] + registry[b.toInt()] }
+        operations["addi"] = { a, b, c, registry -> registry[c.toInt()] = registry[a.toInt()] + b }
+
+        operations["mulr"] = { a, b, c, registry -> registry[c.toInt()] = registry[a.toInt()] * registry[b.toInt()] }
+        operations["muli"] = { a, b, c, registry -> registry[c.toInt()] = registry[a.toInt()] * b }
+
+        operations["banr"] = { a, b, c, registry -> registry[c.toInt()] = registry[a.toInt()].and(registry[b.toInt()]) }
+        operations["bani"] = { a, b, c, registry -> registry[c.toInt()] = registry[a.toInt()].and(b) }
+
+        operations["borr"] = { a, b, c, registry -> registry[c.toInt()] = registry[a.toInt()].or(registry[b.toInt()]) }
+        operations["bori"] = { a, b, c, registry -> registry[c.toInt()] = registry[a.toInt()].or(b) }
+
+        operations["setr"] = { a, b, c, registry -> registry[c.toInt()] = registry[a.toInt()] }
+        operations["seti"] = { a, b, c, registry -> registry[c.toInt()] = a }
+
+        operations["gtir"] = { a, b, c, registry -> registry[c.toInt()] = if (a > registry[b.toInt()]) 1 else 0 }
+        operations["gtri"] = { a, b, c, registry -> registry[c.toInt()] = if (registry[a.toInt()] > b) 1 else 0 }
+        operations["gtrr"] = { a, b, c, registry -> registry[c.toInt()] = if (registry[a.toInt()] > registry[b.toInt()]) 1 else 0 }
+
+        operations["eqir"] = { a, b, c, registry -> registry[c.toInt()] = if (a == registry[b.toInt()]) 1 else 0 }
+        operations["eqri"] = { a, b, c, registry -> registry[c.toInt()] = if (registry[a.toInt()] == b) 1 else 0 }
+        operations["eqrr"] = { a, b, c, registry -> registry[c.toInt()] = if (registry[a.toInt()] == registry[b.toInt()]) 1 else 0 }
 
 
-    return operations
+        return operations
+    }
 }
---
+++ src/main/kotlin/adventofcode/19.kt
@@ -0,0 +1,145 @@
+package adventofcode
+
+import java.io.File
+
+fun main(args: Array<String>) {
+
+    val operationCodes = OperationCodes()
+
+    val (operationResults, instructions) = operationCodes.parseInput()
+    val operations = operationCodes.getOperations()
+
+    val rawInput = File("src/main/resources/19.txt").readLines()
+
+    val (ipRegistry, programInstructions) = Day19.parseInput(rawInput)
+
+    //println(ipRegistry)
+    //println(programInstructions)
+
+    // Part one.
+    var registry = mutableListOf(0L, 0L, 0L, 0L, 0L, 0L)
+    var ipValue = 0L
+    executeInstructions(registry, ipRegistry, ipValue, programInstructions, operations)
+    println(registry)
+
+    // Part two.
+    registry = mutableListOf(1L, 0L, 0L, 0L, 0L, 0L)
+    ipValue = 0L
+    // To big loop (fast forward inner loop..)
+    executeInstructions(registry, ipRegistry, ipValue, programInstructions, operations, false)
+    println(registry)
+    // 27941760
+
+}
+
+private fun executeInstructions(
+    registry: MutableList<Long>,
+    ipRegistry: Int,
+    ipValueStart: Long,
+    programInstructions: List<NamedInstruction>,
+    operations: MutableMap<String, (Long, Long, Long, MutableList<Long>) -> Unit>,
+    debug: Boolean = false
+) {
+    var ipValue = ipValueStart
+    do {
+
+        registry[ipRegistry] = ipValue
+
+        val registryBeforeOperation = registry.toMutableList()
+        val ins = programInstructions[ipValue.toInt()]
+        operations[ins.opName]!!.invoke(ins.a, ins.b, ins.c, registry)
+
+        if (debug) {
+            printProgramState(ipValue, registryBeforeOperation, ins, registry)
+        }
+
+        ipValue = registry[ipRegistry] + 1
+
+        // Fast forward hack.
+        if (ipValue == 4L && registry[3] == 1L && registry[5] == 10551264L) {
+            // Fast forward
+            if (registry[5] % registry[1] == 0L) {
+                registry[2] = 10551264L
+                registry[3] = 10551264L
+            } else {
+                registry[3] = 10551264L
+            }
+
+        }
+    } while (ipValue < programInstructions.size)
+}
+
+fun printProgramState(
+    ipValue: Long,
+    registryBeforeOperation: MutableList<Long>,
+    ins: NamedInstruction,
+    registry: MutableList<Long>
+) {
+    println("ip=$ipValue $registryBeforeOperation ${ins.opName} ${ins.a} ${ins.b} ${ins.c} $registry")
+}
+
+data class NamedInstruction(val opName: String, val a: Long, val b: Long, val c: Long)
+
+object Day19 {
+    fun parseInput(rawInput: List<String>): Pair<Int, List<NamedInstruction>> {
+        val instructions = mutableListOf<NamedInstruction>()
+
+        val ipRegistry = Integer.valueOf(rawInput[0].split(" ")[1])
+
+        for (i in 1 until rawInput.size) {
+            instructions.add(parseInstruction(rawInput[i]));
+        }
+
+        return Pair(ipRegistry, instructions)
+    }
+
+    private fun parseInstruction(input: String): NamedInstruction {
+        val list = input.split(" ").toList()
+        return NamedInstruction(
+            list[0],
+            Integer.valueOf(list[1]).toLong(),
+            Integer.valueOf(list[2]).toLong(),
+            Integer.valueOf(list[3]).toLong()
+        )
+    }
+}
+/**
+addi 4 16 4
+seti 1 1 1
+while (r3 <= r5) {
+    seti 1 7 3                                                                      r3 = 1
+    ip=3 [0, 1, 0, 91752, 3, 10551264] mulr 1 3 2 [0, 1, 91752, 91752, 3, 10551264]	r2 = r1 * r3
+    ip=4 [0, 1, 91752, 91752, 4, 10551264] eqrr 2 5 2 [0, 1, 0, 91752, 4, 10551264]	r2 = r2 == r5 ? 1 : 0
+    ip=5 [0, 1, 0, 91752, 5, 10551264] addr 2 4 4 [0, 1, 0, 91752, 5, 10551264]		*r4 += r2
+    ip=6 [0, 1, 0, 91752, 6, 10551264] addi 4 1 4 [0, 1, 0, 91752, 7, 10551264]		*r4++
+    (when r2 == 1 -> addr 1 0 0 r0 += r1)
+    ip=8 [0, 1, 0, 91752, 8, 10551264] addi 3 1 3 [0, 1, 0, 91753, 8, 10551264]		r3++
+    ip=9 [0, 1, 0, 91753, 9, 10551264] gtrr 3 5 2 [0, 1, 0, 91753, 9, 10551264]		r2 = r3 > r5 ? 1 : 0
+    ip=10 [0, 1, 0, 91753, 10, 10551264] addr 4 2 4 [0, 1, 0, 91753, 10, 10551264]	*r4 += r2
+    ip=11 [0, 1, 0, 91753, 11, 10551264] seti 2 3 4 [0, 1, 0, 91753, 2, 10551264]	*r4 = 2
+} when loop ends -> r0 += r1 		-> [1, 1, 1, 10551265, 12, 10551264]
+addi 1 1 1 <- r1++
+gtrr 1 5 2
+addr 2 4 4 GOTO 2 r2 needs to be = 1 -> r1 > r5 -> r1 = 10551265
+seti 1 6 4 ********
+mulr 4 4 4
+addi 5 2 5
+mulr 5 5 5
+mulr 4 5 5
+muli 5 11 5
+addi 2 1 2
+mulr 2 4 2
+addi 2 6 2
+addr 5 2 5
+addr 4 0 4
+seti 0 0 4
+setr 4 5 2
+mulr 2 4 2
+addr 4 2 2
+mulr 4 2 2
+muli 2 14 2
+mulr 2 4 2
+addr 5 2 5
+seti 0 5 0
+seti 0 2 4
+ */
