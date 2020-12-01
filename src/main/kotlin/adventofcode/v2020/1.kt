47454e8dff990389ff584843ee2714f3c84bc19e
Magnus Andersson <mandersson@invidi.se>
2020-12-01 15:33:04 +0100

    Day 1.

--- src/main/kotlin/adventofcode/v2020/1.kt
+++ src/main/kotlin/adventofcode/v2020/1.kt
@@ -1,27 +1,46 @@
 package adventofcode.v2020
 
 import adventofcode.util.FileParser
-import adventofcode.v2019.shared.IntCodeComputer
+import java.util.Optional
+import java.util.Optional.empty
+import java.util.Optional.of
+import kotlin.system.measureTimeMillis
 
-fun main(args: Array<String>) {
+fun main() {
 
-    val modulesMasses = FileParser.getFileRows(2020, "1.txt").map { Integer.valueOf(it) }
+    val entries = FileParser.getFileRows(2020, "1.txt").map { Integer.valueOf(it) }
 
-    // Fuel sum.
-    println(modulesMasses.map(Day1::fuel).sum())
 
-    // Fuel sum with fuel taken into account.
-    println(modulesMasses.map { Day1.fuelWithFuelTakenIntoAccount(it) }.sum())
+    val time1 = measureTimeMillis {
+        println("answer part 1: " + Day1.get2020EntriesProduct(entries))
+    }
+    println("Time: $time1 ms")
+
+    val time2 = measureTimeMillis {
+        println("answer part 2: " + Day1.get2020ThreeEntriesProduct(entries))
+    }
+    println("Time: $time2 ms")
 }
 
 object Day1 {
+    fun get2020EntriesProduct(entries: List<Int>): Optional<Int> {
 
-    fun fuel(mass: Int): Int {
-        return mass / 3 - 2
+        for (index in entries.indices) {
+            val first = entries[index]
+            entries.drop(index).forEach { if (first + it == 2020) return of(first * it) }
+        }
+        return empty()
     }
 
-    tailrec fun fuelWithFuelTakenIntoAccount(mass: Int, accFuel: Int = 0): Int {
-        val fuel = fuel(mass)
-        return if (fuel <= 0) accFuel else fuelWithFuelTakenIntoAccount(fuel, accFuel + fuel)
+    fun get2020ThreeEntriesProduct(entries: List<Int>): Optional<Int> {
+
+        for (index1 in entries.indices) {
+            val first = entries[index1]
+            for (index2 in (index1 + 1) until entries.size) {
+                val second = entries[index2]
+                entries.drop(index2).forEach { if (first + second + it == 2020) return of(first * second * it) }
+            }
+        }
+        return empty()
     }
 }
