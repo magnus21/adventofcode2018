d6aacbc619861e6a38d5d33a717aa89ced7b3d86
Magnus Andersson <magnus21@gmail.com>
2022-12-24 09:13:54 +0100

    Day 16, part 2, not 100% happy with it.

--- src/main/kotlin/adventofcode/v2022/16.kt
+++ src/main/kotlin/adventofcode/v2022/16.kt
@@ -12,8 +12,8 @@ object Day16 {
     fun main(args: Array<String>) {
         val valves = parseInput(FileParser.getFileRows(2022, "16.txt"))
 
-        // to low: 1770, 1780
-        //printResult("part 1") { part1(valves) }
+        printResult("part 1") { part1(valves) }
+        // Brut forced part 2.
         printResult("part 2") { part2(valves) }
     }
 
@@ -62,101 +62,21 @@ object Day16 {
         val nonOpenedValves = mutableListOf<ValveRoom>()
         nonZeroRateValves.forEach(nonOpenedValves::add)
 
-        val myTrail = mutableListOf(TrailStep("AA", 0, 0, 0))
-        val elephantTrail = mutableListOf(TrailStep("AA", 0, 0, 0))
-
-        val totalTime = 26
-        var myTime = 0
-        var elephantTime = 0
-        while (nonOpenedValves.isNotEmpty() && myTime < totalTime && elephantTime < totalTime) {
-
-            val myChoices = nonOpenedValves.map {
-                findShortestPathPartToValve(myTrail.last(), it, optimizedValvesMap)
-            }.map { it.trail.last() }
-
-            val myPath = myChoices
-                .sortedBy {
-                    (totalTime - (myTime + it.time + 1)) * optimizedValvesMap[it.valve]!!.rate
-                }
-                .reversed()
-                .first()
-
-
-            myTime = openValve(myPath, myTime, totalTime, myTrail)
-            if (myTime < totalTime) {
-                nonOpenedValves.remove(optimizedValvesMap[myPath.valve]!!)
-            }
-
-
-            val elephantsChoices = nonOpenedValves.map {
-                findShortestPathPartToValve(elephantTrail.last(), it, optimizedValvesMap)
-            }.map { it.trail.last() }
-
-            val elephantsPath = elephantsChoices
-                .sortedBy {
-                    (totalTime - (elephantTime + it.time + 1)) * optimizedValvesMap[it.valve]!!.rate
-                }
-                .reversed()
-                .firstOrNull()
-
-            if (elephantsPath != null) {
-                elephantTime = openValve(elephantsPath, elephantTime, totalTime, elephantTrail)
-                if (elephantTime < totalTime) {
-                    nonOpenedValves.remove(optimizedValvesMap[elephantsPath.valve]!!)
-                }
-            }
-        }
-
-        return findShortestPathPart2(valves)
-    }
-
-    private fun openValve(
-        destination: TrailStep,
-        startTime: Int,
-        totalTime: Int,
-        trail: MutableList<TrailStep>
-    ): Int {
-        var time = startTime
-        val newTime = time + destination.time
-        val last = trail.last()
-        if (newTime < totalTime) {
-            time += destination.time
-            trail.add(
-                TrailStep(
-                    destination.valve,
-                    destination.time + 1,
-                    destination.nextRate,
-                    last.totalRate + last.nextRate * (totalTime - time),
-                    true
-                )
-            )
-            return newTime + 1
-        } else {
-            println("Didn't reach valve in time")
-            trail.add(
-                TrailStep(
-                    last.valve,
-                    destination.time -(newTime - totalTime),
-                    last.nextRate,
-                    last.totalRate + last.nextRate * (totalTime - time)
-                )
-            )
-            return totalTime
-        }
+        return findShortestPathPart2(optimizedValvesMap, nonZeroRateValves)
     }
 
     private fun findShortestPathPartToValve(
-        startValve: TrailStep,
-        toValve: ValveRoom,
+        startValve: String,
+        toValve: String,
         valveMap: Map<String, ValveRoom>
     ): ValvePath {
         val queue = Queue<ValvePath>()
-        queue.enqueue(ValvePath(listOf(TrailStep(startValve.valve, 0, 0, 0))))
+        queue.enqueue(ValvePath(listOf(TrailStep(startValve, 0, 0, 0))))
 
         var shortestTime = Int.MAX_VALUE
         var shortestPath: ValvePath? = null
         val visited = mutableMapOf<String, Int>()
-        visited[startValve.valve] = 0
+        visited[startValve] = 0
 
         while (queue.isNotEmpty()) {
             val from = queue.dequeue()!!
@@ -175,7 +95,7 @@ object Day16 {
 
             newPaths.forEach {
                 val lastStep = it.trail.last()
-                if (lastStep.valve == toValve.label && lastStep.time < shortestTime) {
+                if (lastStep.valve == toValve && lastStep.time < shortestTime) {
                     shortestTime = lastStep.time
                     shortestPath = it
                 }
@@ -318,11 +238,14 @@ object Day16 {
         val openedValves: Set<String> = setOf()
     )
 
-    private fun findShortestPathPart2(valves: List<ValveRoom>, maxTime: Int = 26): Int {
+    private fun findShortestPathPart2(
+        valveMap: Map<String, ValveRoom>,
+        nonZeroValves: List<ValveRoom>,
+        maxTime: Int = 26
+    ): Int {
 
-        val maxRate = valves.sumOf { it.rate }
-        val valveMap = valves.associateBy { it.label }
-        val nonZeroRateValves = valves.count { it.rate > 0 }
+        val maxRate = nonZeroValves.sumOf { it.rate }
+        val nonZeroRateValves = nonZeroValves.size
 
         val start = ValvePathWithElephant(
             Pair(
@@ -332,138 +255,124 @@ object Day16 {
             setOf()
         )
 
+        val nonZeroValvesMap = nonZeroValves
+            .plus(valveMap["AA"]!!)
+            .map { vFrom ->
+                val leadsTo = nonZeroValves
+                    .filter { vFrom != it }
+                    .map { vTo ->
+                        val valvePath = findShortestPathPartToValve(vFrom.label, vTo.label, valveMap).trail.last()
+                        Pair(vTo.label, valvePath.time)
+                    }
+                ValveRoom(vFrom.label, vFrom.rate, leadsTo.toMutableList())
+            }.associateBy { it.label }
+
         val queue = Queue<ValvePathWithElephant>()
         queue.enqueue(start)
 
-        var highestRateAtEnd = 0
+        var highestRateAtEnd = 0 //1971 //0
 
         while (queue.isNotEmpty()) {
             val from = queue.dequeue()!!
+            val myLastStep = from.trails.first.last()
+            val elephantsLastStep = from.trails.second.last()
 
-            /*val newPaths = getNewPaths(from, valveMap, maxTime)
+            val newPaths = nonZeroValvesMap[myLastStep.valve]!!.leadsTo
+                .filter { !from.openedValves.contains(it.first) }
+                .sortedByDescending { nonZeroValvesMap[it.first]!!.rate }
+                .flatMap { myLeadTo ->
+                    val toValve = nonZeroValvesMap[myLeadTo.first]!!
+                    val newTime = myLastStep.time + myLeadTo.second + 1
 
-            newPaths
-                .forEach {
-                    val last = it.trail.last()
-                    val projectedRate = last.totalRate + (maxTime - last.time) * last.nextRate
-                    val projectedMax = last.totalRate + (maxTime - last.time) * maxRate
+                    val myStep = if (newTime < maxTime) TrailStep(
+                        toValve.label,
+                        newTime,
+                        myLastStep.nextRate + toValve.rate,
+                        myLastStep.totalRate + myLastStep.nextRate * (myLeadTo.second + 1),
+                        true
+                    ) else TrailStep(
+                        myLastStep.valve,
+                        maxTime,
+                        myLastStep.nextRate,
+                        myLastStep.totalRate + myLastStep.nextRate * (maxTime - myLastStep.time)
+                    )
 
-                    if ((nonZeroRateValves == it.openedValves.size) && highestRateAtEnd <= projectedRate) {
-                        highestRateAtEnd = projectedRate
-                        println("New high (all valves opened): $it, $projectedRate")
-                    } else if (projectedMax > highestRateAtEnd) {
-                        if (last.time < maxTime) {
-                            queue.enqueue(it)
+                    if (from.openedValves.size + 1 == nonZeroRateValves) {
+                        listOf(
+                            ValvePathWithElephant(
+                                Pair(from.trails.first.plus(myStep), from.trails.second),
+                                from.openedValves.plus(myStep.valve)
+                            )
+                        )
+                    } else {
+                        val valvesLeft = nonZeroValvesMap[elephantsLastStep.valve]!!.leadsTo
+                            .filter { !from.openedValves.contains(it.first) }
+                            .sortedByDescending { nonZeroValvesMap[it.first]!!.rate }
+                            .filter { it.first != myStep.valve }
+
+                        if (valvesLeft.isEmpty()) {
+                            listOf(
+                                ValvePathWithElephant(
+                                    Pair(from.trails.first.plus(myStep), from.trails.second),
+                                    from.openedValves.plus(myStep.valve)
+                                )
+                            )
                         }
-                        if (last.time == maxTime && highestRateAtEnd < last.totalRate) {
-                            highestRateAtEnd = last.totalRate
-                            println("New high: ${last.totalRate}, $it")
+                        valvesLeft.map { eLeadTo ->
+                            val eToValve = nonZeroValvesMap[eLeadTo.first]!!
+                            val eNewTime = elephantsLastStep.time + eLeadTo.second + 1
+                            if (eNewTime < maxTime) TrailStep(
+                                eToValve.label,
+                                eNewTime,
+                                elephantsLastStep.nextRate + eToValve.rate,
+                                elephantsLastStep.totalRate + elephantsLastStep.nextRate * (eLeadTo.second + 1),
+                                true
+                            ) else TrailStep(
+                                elephantsLastStep.valve,
+                                maxTime,
+                                elephantsLastStep.nextRate,
+                                elephantsLastStep.totalRate + elephantsLastStep.nextRate * (maxTime - elephantsLastStep.time)
+                            )
+                        }.map {
+                            ValvePathWithElephant(
+                                Pair(from.trails.first.plus(myStep), from.trails.second.plus(it)),
+                                from.openedValves.plus(myStep.valve).plus(it.valve)
+                            )
                         }
                     }
                 }
-            queue.sortQueue(compareBy<ValvePath> { it.openedValves.size }.reversed())
-            */
+            newPaths
+                .forEach {
+                    val myLast = it.trails.first.last()
+                    val eLast = it.trails.second.last()
+
+                    val projectedMax = myLast.totalRate + eLast.totalRate +
+                            (maxTime - myLast.time) * maxRate +
+                            (maxTime - eLast.time) * maxRate
+
+                    val myTotalRate = myLast.totalRate + (maxTime - myLast.time) * myLast.nextRate
+                    val eTotalRate = eLast.totalRate + (maxTime - eLast.time) * eLast.nextRate
+                    val totalProjectedRate = myTotalRate + eTotalRate
+
+                    if (highestRateAtEnd < totalProjectedRate) {
+                        highestRateAtEnd = totalProjectedRate
+                        println("New high: ${totalProjectedRate}, $it")
+                    }
+
+                    if (nonZeroRateValves == it.openedValves.size) {
+                        if (highestRateAtEnd <= totalProjectedRate) {
+                            highestRateAtEnd = totalProjectedRate
+                            println("New high (all valves opened): $it, $highestRateAtEnd")
+                        }
+                    } else if (highestRateAtEnd < projectedMax) {
+                        if (myLast.time < maxTime || eLast.time < maxTime) {
+                            queue.enqueue(it)
+                        }
+                    }
+                }
+            //queue.sortQueue(compareBy<ValvePathWithElephant> { it.openedValves.size }.reversed())
         }
         return highestRateAtEnd
-    }
-
-
-    private fun tryOpenValve(
-        leadTo: Pair<String, Int>,
-        lastStep: TrailStep,
-        valveMap: Map<String, ValveRoom>,
-        openedValves: Map<String, Int>,
-        maxTime: Int
-    ): TrailStep? {
-        val newRoom = valveMap[leadTo.first]!!
-        val timeToNewRoom = leadTo.second
-        val newTimeOpenValve = lastStep.time + timeToNewRoom + 1
-
-        if (newTimeOpenValve <= maxTime && !openedValves.contains(newRoom.label)) {
-            return TrailStep(
-                newRoom.label,
-                newTimeOpenValve,
-                lastStep.nextRate + newRoom.rate,
-                lastStep.totalRate + (timeToNewRoom + 1) * lastStep.nextRate,
-                true
-            )
-        }
-        return null
-    }
-
-    private fun getNewPaths(
-        from: ValvePathWithElephant,
-        openedValves: Map<String, Int>,
-        valveMap: Map<String, ValveRoom>,
-        maxTime: Int
-    ) {
-
-        /* val lastStep = from.trails.first.last()
-         val secondLastStep = from.trails.first.dropLast(1).lastOrNull()
-         val lastRoom = valveMap[lastStep.valve]!!
-
-         val elephantLastStep = from.trails.second.last()
-         val elephantSecondLastStep = from.trails.second.dropLast(1).lastOrNull()
-         val elephantLastRoom = valveMap[elephantLastStep.valve]!!
-
-         val newPaths = lastRoom.leadsTo
-             .flatMap { leadTo ->
-                 val newSteps = mutableListOf<Pair<TrailStep,TrailStep>>()
-
-                 val yourNextStep = tryOpenValve(leadTo, lastStep,valveMap, openedValves, maxTime)
-                 if(yourNextStep != null) {
-
-                     elephantLastRoom.leadsTo.flatMap {
-                         val elephantsNextSteps = tryOpenValve(it, elephantLastStep, valveMap, openedValves, maxTime)
-                         //elephantsNextSteps
-                     }
-                 }
-
-
-                 val newRoom = valveMap[leadTo.first]!!
-                 val timeToNewRoom = leadTo.second
-                 val newTimeOpenValve = lastStep.time + timeToNewRoom + 1
-
-                 if (newTimeOpenValve <= maxTime && !openedValves.contains(newRoom.label)) {
-                    val yourStep =  TrailStep(
-                             newRoom.label,
-                             newTimeOpenValve,
-                             lastStep.nextRate + newRoom.rate,
-                             lastStep.totalRate + (timeToNewRoom + 1) * lastStep.nextRate,
-                             true
-                         )
-                     /*elephantLastRoom.leadsTo
-                         .filter { it.first != newRoom.label }
-                         .map {
-
-                         }*/
-
-
-
-                 }
-
-                 val newTimeDontOpenValve = lastStep.time + timeToNewRoom
-                 if (secondLastStep == null || newRoom.label != secondLastStep.valve || lastStep.openedValve) {
-                     val time = if (newTimeDontOpenValve > maxTime) maxTime else newTimeDontOpenValve
-                     val timeStep = if (newTimeDontOpenValve > maxTime) maxTime - lastStep.time else timeToNewRoom
-                     val room = if (newTimeDontOpenValve > maxTime) lastRoom.label else newRoom.label
-                     newSteps.add(
-                         TrailStep(
-                             room,
-                             time,
-                             lastStep.nextRate,
-                             lastStep.totalRate + timeStep * lastStep.nextRate
-                         )
-                     )
-                 }
-                 newSteps
-             }
-             .map {
-                 val newOpenedValves = if (it.openedValve) openedValves.plus(it.valve) else openedValves
-                 ValvePath(trail.plus(it), newOpenedValves)
-             }.filter { it.trail.last().time <= maxTime }
-         return newPaths
-         */
     }
 
     private data class ValveRoom(val label: String, val rate: Int, val leadsTo: MutableList<Pair<String, Int>>)
